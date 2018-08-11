484e6c1fe52b44fd774e15dcbf278924fdbf2930
Marc Vila <marcvilagomez@gmail.com>
2018-08-11 18:34:36 +0200

    Adding BSSID info

--- main/main.ino
+++ main/main.ino
@@ -3,10 +3,7 @@
 void setup() {
   Serial.begin(115200);
 
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
+  setup_wifi();
 
   Serial.println("Setup done");
 }
@@ -14,29 +11,7 @@ void setup() {
 void loop() {
   Serial.println("scan start");
 
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0)
-    Serial.println("no networks found");
-  else
-  {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i)
-    {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
-      delay(10);
-    }
-  }
-  Serial.println("");
+  scan_wifi();
 
   // Wait a bit before scanning again
   delay(5000);
